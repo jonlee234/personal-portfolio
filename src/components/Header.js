f5176d9473a9e6621f5a13a2122b2f2985d9d77d
Jonathane Leavell <jonathaneleavell@gmail.com>
2021-05-11 19:57:51 -0400

    initial starting Point

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+
+function Header() {
+  return (
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Navbar.Brand href="#home">Jonathane Leavell</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto">
+          <Nav.Link href="#home">About</Nav.Link>
+          <Nav.Link href="#link">Projects</Nav.Link>
+          <Nav.Link href="#link">Resume</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
+
+export default Header;
